a7d0ec2ad4fb8bff67c1e47d7e0f66d2921532cc
Clement Fortin <clement.fortin@valtech.com>
2020-03-22 16:43:05 -0400

    added db migration (flask-migrate with Alembic) and completed todo functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from sys import exc_info
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -9,20 +10,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}'
 
-db.create_all()
-
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all()
-    )
+    return render_template('index.html', data=Todo.query.order_by('id').all())
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    error = False
+    body={}
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
 
 @app.route('/todos/create', methods=['POST'])
 def create():
